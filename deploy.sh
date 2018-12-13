ab2e222e314d4baf980e6889b1a15a0e4dc161a0
sontek <sontek@yeah.net>
2018-12-13 10:08:45 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-echo "start build"
+echo "=================start build==================="
 gitbook build >> /dev/null
 
-echo "clean files"
+echo "=================clean files==================="
 git checkout gh-pages
 git rm --cached -r . >> /dev/null
 git clean -df >> /dev/null
 
+echo "*~" > .gitignore
 echo "_book" >> .gitignore
 
-echo "copy static file and commit ..."
+echo "================copy static file and commit ==============="
 cp -r _book/* .
 rm -rf deploy.sh
 git add .
@@ -18,7 +19,7 @@ git commit -m "publish book"
 git push origin gh-pages:gh-pages
 
 git checkout master
-echo "success!!!"
+echo "=================success!!!====================="
 
 
 
